1c3b04f3459aab5ab10ff22d05a94d44aa844021
williamhhk <william.han@gmail.com>
2016-10-19 19:32:25 -0700

    Add SCCM AddMembershipRule

--- WinRM.CIM/WinRM.cs
+++ WinRM.CIM/WinRM.cs
@@ -14,9 +14,8 @@ namespace WinRM.CIM
             SecureString securePassword = new SecureString();
             Array.ForEach(passWord.ToArray(), securePassword.AppendChar);
             securePassword.MakeReadOnly();
-            CimCredential credentials = new CimCredential(PasswordAuthenticationMechanism.Default, domain, userName, securePassword);
             WSManSessionOptions sessionOptions = new WSManSessionOptions();
-            sessionOptions.AddDestinationCredentials(credentials);
+            sessionOptions.AddDestinationCredentials(new CimCredential(PasswordAuthenticationMechanism.Default, domain, userName, securePassword));
             return CimSession.Create(hostName, sessionOptions);
          }
 
@@ -25,5 +24,36 @@ namespace WinRM.CIM
             return CimSession.Create(null).QueryInstances(nameSpace, "WQL", queryExpression);
         }
 
-     }
+        public static IEnumerable<CimInstance> GetAllCollections(string hostName, string userName, string passWord, string domain, string cmSite, string collectionId)
+        {
+            CimSession session = Connect(hostName, userName, passWord, domain);
+            // Get All Collection
+            //  Add Exception catching chain
+            return session.EnumerateInstances($"root\\sms\\site_{cmSite}", "SMS_Collection")
+                .Where(i => i.CimInstanceProperties["CollectionId"].Value.ToString().Contains(collectionId)).ToList();
+        }
+
+        public static void AddMembershipRule(string hostName, string userName, string passWord, string domain, string cmSite, string collectionId)
+        {
+            CimSession session = Connect(hostName, userName, passWord, domain);
+            // Get All Collection
+            //  Add Exception catching chain
+            var collectionInstance = session.EnumerateInstances($"root\\sms\\site_{cmSite}", "SMS_Collection")
+                .Where(i => i.CimInstanceProperties["CollectionId"].Value.ToString().Contains(collectionId)).ToList();
+
+            if (!collectionInstance.Any()) return;
+
+            var ruleInstance = new CimInstance(session.GetClass($"root\\sms\\site_{cmSite}", "SMS_CollectionRuleDirect"));
+            ruleInstance.CimInstanceProperties["RuleName"].Value = "";
+            ruleInstance.CimInstanceProperties["ResourceClassName"].Value = "SMS_R_System";
+            ruleInstance.CimInstanceProperties["ResourceID"].Value = 1;
+
+            var parameters = new CimMethodParametersCollection
+            {
+                CimMethodParameter.Create("collectionRule", ruleInstance, CimType.Instance,0)
+            };
+            CimMethodResult result = session.InvokeMethod(collectionInstance.FirstOrDefault(), "AddMembershipRule", parameters);
+        }
+
+    }
 }
