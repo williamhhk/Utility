63432411ba9a7302b1532cfa612b3f9e6ed424d2
williamhhk <william.han@gmail.com>
2016-10-19 18:46:05 -0700

    Add Microsoft.Management.Infrastructure sample

---
+++ WimRM.CIM.Tests/Test_WimRM.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Linq;
+namespace WinRM.CIM.Tests
+{
+    [TestFixture]
+    public class Test_WimRM
+    {
+        [Test]
+        public void Test_No_Error_Query_Results()
+        {
+            var results = WinRM.Query("root\\cimv2", "SELECT * FROM Win32_Volume").ToList();
+        }
+           
+    }
+}
---
+++ WinRM.CIM/WinRM.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Management.Infrastructure;
+using Microsoft.Management.Infrastructure.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+
+namespace WinRM.CIM
+{
+    public static class WinRM
+    {
+        public static CimSession Connect(string hostName, string userName, string passWord, string domain)
+        {
+            SecureString securePassword = new SecureString();
+            Array.ForEach(passWord.ToArray(), securePassword.AppendChar);
+            securePassword.MakeReadOnly();
+            CimCredential credentials = new CimCredential(PasswordAuthenticationMechanism.Default, domain, userName, securePassword);
+            WSManSessionOptions sessionOptions = new WSManSessionOptions();
+            sessionOptions.AddDestinationCredentials(credentials);
+            return CimSession.Create(hostName, sessionOptions);
+         }
+
+        public static IEnumerable<CimInstance> Query(string nameSpace, string queryExpression)
+        {
+            return CimSession.Create(null).QueryInstances(nameSpace, "WQL", queryExpression);
+        }
+
+     }
+}
